10f80de0c4131242a61b91e8ad26d0594726b3df
Suzanne Schuber <suzanneschuber@gmail.com>
2023-08-04 09:28:18 -0700

    Add files for controllers and models.

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,17 +1,12 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Sillystrings.Solutions.Models;
+using Factory.Models;
 
-namespace Sillystrings.Solutions.Controllers;
+namespace Factory.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    
 
     public IActionResult Index()
     {
