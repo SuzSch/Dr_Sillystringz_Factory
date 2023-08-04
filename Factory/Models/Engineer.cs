486e197a5425ff4733c73d3b4f6789764105e085
Suzanne Schuber <suzanneschuber@gmail.com>
2023-08-04 10:01:30 -0700

    Create all models, database migration and update.

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public int EngineerId { get; set; }
+    public string EngineerName { get; set; }
+    public string HireDate { get; set; }
+    public string FavoriteSnack { get; set; }
+
+    public List<EngineerMachine> JoinEntities { get; }
+  }
+}
