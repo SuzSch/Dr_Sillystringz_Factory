c4eec0a026a9c6f2a600b8d8783d13b69aed2b56
Suzanne Schuber <suzanneschuber@gmail.com>
2023-08-04 15:37:06 -0700

    update create.cshtml to make required messages and form look better and stand out more.

---
+++ Factory/Migrations/20230804221346_ChangeFirstEngineer.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sillystrings.Solutions.Migrations
+{
+    public partial class ChangeFirstEngineer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Engineers",
+                keyColumn: "HireDate",
+                keyValue: null,
+                column: "HireDate",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HireDate",
+                table: "Engineers",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Engineers",
+                keyColumn: "FavoriteSnack",
+                keyValue: null,
+                column: "FavoriteSnack",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FavoriteSnack",
+                table: "Engineers",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Engineers",
+                keyColumn: "EngineerName",
+                keyValue: null,
+                column: "EngineerName",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EngineerName",
+                table: "Engineers",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "HireDate",
+                table: "Engineers",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FavoriteSnack",
+                table: "Engineers",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EngineerName",
+                table: "Engineers",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
