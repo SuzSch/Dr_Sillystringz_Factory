d7ad93665d44d4913ae9156380f064babb3332f4
Suzanne Schuber <suzanneschuber@gmail.com>
2023-08-05 13:22:01 -0700

    Add delete methods for machines and engineers, WIP views for each.

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -92,5 +92,13 @@ namespace Factory.Controllers
       }
       return RedirectToAction("Details", new { id = machine.MachineId });
     }
+    [HttpPost]
+    public ActionResult DeleteEngineer(int joinId)
+    {
+      EngineerMachine joinEntry = _db.EngineerMachines.FirstOrDefault(entry => entry.EngineerMachineId == joinId);
+      _db.EngineerMachines.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index", "Home");
+    }
   }
 }
